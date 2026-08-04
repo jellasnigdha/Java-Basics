3a40cd1c664b3421ba60510fbcfc01604b9c09b0
Jella Snigdha <jellasnigdha0@gmail.com>
2026-08-04 20:27:45 +0530

    Implement Armstrong number checker in Armstrong.java
    Fix syntax errors and implement Armstrong number logic.

---
+++ Armstrong.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ArmstrongNumber{ 
+public static void main(String[]args) {
+  Scanner sc = new Scanner(System.in)
+int n = sc,nextInt();
+  int temp = num;
+  int sum = 0;
+  while ( temp>0) {
+int digit = temp %10;
+  sum +=  digit *digit * digit*;
+    temp /=10;
+  } if (sum ==n)
+    System.out.println("Armstrong");
+  else
+    System.out.println("Not Armstrong");
+  )
+}
+    
+  

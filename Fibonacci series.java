139b70d29634cc288efe500fa6bd3b54ed653e73
Jella Snigdha <jellasnigdha0@gmail.com>
2026-08-04 21:34:56 +0530

    Implement Fibonacci series in Java
    Fix syntax errors and implement Fibonacci series logic.

---
+++ Fibonacci series.java
@@ -0,0 +1,15 @@
+int java.util.Scanner;
+public class FibonacciSeries {
+public static void main(String [] args){ 
+  Scaneer sc = new Scanner(System.in);
+  int n = sc.nextInt();
+  int first = 0;
+  int second = 1;
+  for(int i =1, i<=n ,i++)
+      System.out.println(first+" ");
+  int next= first + second;
+  first = second;
+  second = next;
+}
+}
+}
